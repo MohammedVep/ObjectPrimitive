16367787d552fd6b798c9737fcd6a21b9ba3eaa5
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-28 07:47:27 -0400

    Completed Create a Simple Object with Primitives Exercise

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+         Person person = new Person();
+         person.name = "Mohammed Vepari";
+         person.age = 28;
+         System.out.println("Person's name is: " + person.name);
+         System.out.println("Person's age is: " + person.age);
+    }
+}
